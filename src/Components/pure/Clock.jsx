73737096564a7172abb055bad126ce00d60fc552
Lohana <Loha@hotmail.com>
2023-04-23 11:51:11 +0200

    Creacion componente Clock- ciclo de vida actualizacion estado del componente, renderizado del componente

--- src/Components/pure/Clock.jsx
+++ src/Components/pure/Clock.jsx
@@ -1,9 +1,42 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import './Clock.scss'
 
 const Clock = () => {
+    const PreState={
+        fecha: new Date(),
+        edad:0,
+        nombre:'Lohana',
+        apellido:'Orellano'
+ }
+
+ const [user, setUser] = useState(PreState);
+
+ useEffect(() => {
+    const intervalEdad = setInterval(() => {
+        actualiceuser();
+        
+    }, 1000);
+    return () => {
+        clearInterval(intervalEdad)
+    };
+ }, );
+
+
+    const actualiceuser = () => {
+        return setUser({
+            fecha:user.fecha,
+            edad:user.edad+1,
+            nombre:user.nombre,
+            apellido:user.apellido
+
+        });
+
+    }
     return (
         <div>
-            
+            <h2>Hora actual: {user.fecha.toLocaleTimeString()}</h2>
+            <h3> {user.name} {user.apellido}</h3>
+            <h1>Edad: {user.edad}</h1>
         </div>
     );
 }
